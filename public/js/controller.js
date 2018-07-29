b9cc49fa1c7836127277582e2a2cca46c6a040ba
3mpirical <3mpirical10510@gmail.com>
2018-07-29 15:47:24 -0600

    refactored delete event handlers

--- public/js/controller.js
+++ public/js/controller.js
@@ -6,13 +6,15 @@ import axios from "axios";
 
 const CTRL = (function(MDL, VIEW) {
 
+
   const initBudget = () => {
     let initIncomes, initExpenses;
+
     // reset total resetTotalObj
     MDL.resetTotalObj();
+
     // retrieve all incomes and expenses
-    // update budget
-    // update income and expense divs
+    // update total object
     MDL.getIncomes()
       .then((res) => {
         res.data.forEach((item) => {
@@ -27,6 +29,8 @@ const CTRL = (function(MDL, VIEW) {
         });
         initExpenses = res.data;
       })
+      // update budget
+      // update income and expense divs
       .then(() => {
         VIEW.updateTotalIncome();
         VIEW.updateTotalExpense();
@@ -46,9 +50,9 @@ const CTRL = (function(MDL, VIEW) {
       .catch((err) => {
         console.log(err);
       });
-
-
   };
+
+
 
   const addItem = () => {
     // 1. Get input data
@@ -89,9 +93,57 @@ const CTRL = (function(MDL, VIEW) {
       });
   };
 
+
+
+  const deleteIncomeAndUpdateAll = (id) => {
+    MDL.deleteIncome(id)
+      .then((res) => {
+        console.log(`DATA WAS REMOVED: ${JSON.stringify(res.data, null, 2)}`);
+
+        // subtract Income from Total Model
+        MDL.subtractTotalIncome(res.data.income);
+        // Update Total Income View
+        VIEW.updateTotalIncome();
+        // Update Net Budget View
+        VIEW.updateNetBudget();
+        // Remove Income Block
+        VIEW.removeIncomeBlock(id);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+
+
+  const deleteExpenseAndUpdateAll = (id) => {
+    MDL.deleteExpense(id)
+      .then((res) => {
+        console.log(`DATA WAS REMOVED: ${JSON.stringify(res.data, null, 2)}`);
+
+        // subtract expense from Total Model
+        MDL.subtractTotalExpense(res.data.expense);
+        // Update Total Expense View
+        VIEW.updateTotalExpense();
+        // Update Net Budget View
+        VIEW.updateNetBudget();
+        // Remove Expense Block
+        VIEW.removeExpenseBlock(id);
+        // Update Percentages (also do this in the addButton function)
+        VIEW.updatePercentages();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+
+
   return {
     initBudget: initBudget,
     addItem: addItem,
+    deleteIncomeAndUpdateAll: deleteIncomeAndUpdateAll,
+    deleteExpenseAndUpdateAll: deleteExpenseAndUpdateAll
   };
 } (MDL, VIEW) );
 
@@ -117,22 +169,7 @@ VIEW.deleteIncomeBtn.addEventListener("click", (event) => {
 
     const id = event.target.id;
 
-    MDL.deleteIncome(id)
-      .then((res) => {
-        console.log(`DATA WAS REMOVED: ${JSON.stringify(res.data, null, 2)}`);
-
-        // subtract Income from Total Model
-        MDL.subtractTotalIncome(res.data.income);
-        // Update Total Income View
-        VIEW.updateTotalIncome();
-        // Update Net Budget View
-        VIEW.updateNetBudget();
-        // Remove Income Block
-        VIEW.removeIncomeBlock(id);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    CTRL.deleteIncomeAndUpdateAll(id);
   }
 });
 
@@ -142,23 +179,6 @@ VIEW.deleteExpenseBtn.addEventListener("click", (event) => {
 
     const id = event.target.id;
 
-    MDL.deleteExpense(id)
-      .then((res) => {
-        console.log(`DATA WAS REMOVED: ${JSON.stringify(res.data, null, 2)}`);
-
-        // subtract expense from Total Model
-        MDL.subtractTotalExpense(res.data.expense);
-        // Update Total Expense View
-        VIEW.updateTotalExpense();
-        // Update Net Budget View
-        VIEW.updateNetBudget();
-        // Remove Expense Block
-        VIEW.removeExpenseBlock(id);
-        // Update Percentages (also do this in the addButton function)
-        VIEW.updatePercentages();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    CTRL.deleteExpenseAndUpdateAll(id);
   }
 });
